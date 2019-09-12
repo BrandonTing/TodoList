4ad4e3024b08e25223cf2d4008994218afc464a7
Brandon <brandon1221t@gmail.com>
2019-09-12 11:58:17 +0800

    Updates

---
+++ precache-manifest.88af3ab68b8fe8998d22cdeda6ea4469.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56094d554f250a73257ba7167238a1a9",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "74fc589b1387c8c2e594",
+    "url": "/todo-list/static/css/main.8563f8c7.chunk.css"
+  },
+  {
+    "revision": "5030a1d9576bd6471562",
+    "url": "/todo-list/static/js/2.7ab5f55f.chunk.js"
+  },
+  {
+    "revision": "74fc589b1387c8c2e594",
+    "url": "/todo-list/static/js/main.f32d4209.chunk.js"
+  },
+  {
+    "revision": "683a11ad9fe58dcd11f9",
+    "url": "/todo-list/static/js/runtime~main.666a91c4.js"
+  },
+  {
+    "revision": "b84fccdc8556af860c81922684d0e01e",
+    "url": "/todo-list/static/media/Alarm_Clock.b84fccdc.mp3"
+  },
+  {
+    "revision": "b6a5d36c984a48861cefa5d4d6a9e641",
+    "url": "/todo-list/static/media/Back_To_The_Wood.b6a5d36c.mp3"
+  },
+  {
+    "revision": "cfb74f7009735e8cb133aabca755de9d",
+    "url": "/todo-list/static/media/Bugle_Tune.cfb74f70.mp3"
+  },
+  {
+    "revision": "fb6a37fc4499013b0ce26c225194aa9a",
+    "url": "/todo-list/static/media/Dinner_Bell_Triangle.fb6a37fc.mp3"
+  },
+  {
+    "revision": "87c87a1c94ea9dc47cc07182edfcfbf8",
+    "url": "/todo-list/static/media/Dusty_Road.87c87a1c.mp3"
+  },
+  {
+    "revision": "5838bb14fd347ccd2cb03e51956d5b2c",
+    "url": "/todo-list/static/media/Galaxy.5838bb14.jpg"
+  },
+  {
+    "revision": "0328f8c81620712329b69ae0e30f08c0",
+    "url": "/todo-list/static/media/Medium_Bell_Ringing_Near.0328f8c8.mp3"
+  },
+  {
+    "revision": "794133c02d11e46ac9fc8b9dcc63fac5",
+    "url": "/todo-list/static/media/Stale_Mate.794133c0.mp3"
+  },
+  {
+    "revision": "08525a71c26db4f594480ea4198ca6a3",
+    "url": "/todo-list/static/media/moon.08525a71.png"
+  }
+]);
